6e2c51c43cec3c7c4b8e691f5790264a14bdcf1d
Naveen K <naveen8972@gmail.com>
2023-08-28 20:12:36 +0530

    added filter button

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,13 +1,21 @@
 import Card from './Card'; 
 import restaurantList from '../utils/mockData';
+import {useState} from 'react';
 
 const Body = () => {
+  const [listOfRestaurant,setlistOfRestaurant] = useState(restaurantList);
     return (
       <div className="body">
-        <div className="search">Search</div>
+        {/* <div className="search">Search</div> */}
+        <button onClick={()=> {
+          let filteredList = listOfRestaurant.filter((element)=>{
+           return element.data.avgRating >= 4
+          })
+          setlistOfRestaurant(filteredList)
+        }}>Filter Rating </button>
         <div className="res-container">
           {
-            restaurantList.map((eachItem) =>  { return (
+            listOfRestaurant.map((eachItem) =>  { return (
             <Card key={eachItem.data.id} resData = {eachItem} />)} )
           }
         </div>
