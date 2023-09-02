e144fb48d256469281e1407bc3b931b4f1c2de59
Naveen K <naveen8972@gmail.com>
2023-09-02 17:20:54 +0530

    Added React Router

--- src/app.js
+++ src/app.js
@@ -2,12 +2,38 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from './component/Header';
 import Body from './component/Body';
+import About from './component/About';
+import Error from "./component/Error";
+import Contact from "./component/Contact";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const AppLayout = () => (
   <div className="app">
     <Header />
-    <Body />
+    <Outlet />
   </div>
 );
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<AppLayout />,
+    children: [
+      {
+        path:"/about",
+        element: <About />
+      },
+      {
+        path:"/",
+        element: <Body />
+      },
+      {
+        path:"/contact",
+        element: <Contact />
+      }
+    ],
+    errorElement: <Error />
+  }
+])
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={router} />);
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,8 +1,8 @@
 import {APP_LOGO} from '../utils/constants';
 import { useState } from "react";
+import { Link } from 'react-router-dom';
 
 const Header = () => 
-
 {
   const [btnName,setBtnName] =  useState("Login")
 return (
@@ -16,9 +16,9 @@ return (
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About Us</Link></li>
+          <li><Link to="/contact">Contact Us</Link></li>
           <li>Cart</li>
           <button className="btn-login" onClick={()=> btnName == "Login" ? setBtnName("Logout") : setBtnName("Login") }>{btnName}</button>
         </ul>
