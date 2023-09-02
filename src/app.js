56026551c3eb0fca1c83e309c13b87af30abaf74
Naveen K <naveen8972@gmail.com>
2023-09-02 22:12:37 +0530

    Added Menu Component

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import Body from './component/Body';
 import About from './component/About';
 import Error from "./component/Error";
 import Contact from "./component/Contact";
+import Menu from "./component/Menu";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const AppLayout = () => (
@@ -30,6 +31,10 @@ const router = createBrowserRouter([
       {
         path:"/contact",
         element: <Contact />
+      },
+      {
+        path:"/restaurants/:resId",
+        element: <Menu />
       }
     ],
     errorElement: <Error />
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,5 @@ export const APP_LOGO =  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9Gc
 export const CARD_IMAGE_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const  SWIGGY_API = "https://corsproxy.io/?https://www.swiggy.com/api/seo/getListing?lat=12.960059122809971&lng=77.57337538383284";
+
+export const SWIGGY_MENU_API = "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=";
