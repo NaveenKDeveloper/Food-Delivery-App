441e659385fcba24a028d4752d39fa7c6a257415
Naveen K <naveen8972@gmail.com>
2023-09-10 11:56:36 +0530

    Added NumberOf MenuItems

--- src/component/MenuCategory.js
+++ src/component/MenuCategory.js
@@ -1,7 +1,7 @@
 const MenuCategory = ({categoryData})=>{
     return(
         <div className="my-3 p-3 bottom-7 shadow-md flex justify-between">
-            <span className="font-extrabold text-sm text-[#3e4152]">{categoryData.title}</span>
+            <span className="font-extrabold text-sm text-[#3e4152]">{categoryData.title} ({categoryData.itemCards.length})</span>
             <span>🔽</span>
         </div>
         
