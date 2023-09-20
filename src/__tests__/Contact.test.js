5c150df52dd0fd57fc5614aa73053d59d997473a
Naveen K <naveen8972@gmail.com>
2023-09-20 23:42:18 +0530

    Added Test Scripts

--- src/__tests__/Contact.test.js
+++ src/__tests__/Contact.test.js
@@ -2,33 +2,36 @@ import { render, screen } from "@testing-library/react"
 import Contact from "../component/Contact"
 import "@testing-library/jest-dom"
 
-test("Should load contact us component",()=>{
 
-    render(<Contact />)
-    const heading = screen.getByRole("heading")
+describe("Contact us test cases",()=>{
+    test("Should load contact us component",()=>{
 
-    expect(heading).toBeInTheDocument()
-})
-
-test("Should load button in contact us component",()=>{
-    render(<Contact/>)
-
-    const button = screen.getByText("Submit")
-    expect(button).toBeInTheDocument()
-})
-
-
-test("Should load placeholder for input in contact us component",()=>{
-    render(<Contact/>)
-
-    const inputName = screen.getByPlaceholderText("name")
-    expect(inputName).toBeInTheDocument()
-})
-
-test("Should load 2 textbox  in contact us component",()=>{
-    render(<Contact/>)
-
-    const inputBox = screen.getAllByRole("textbox")
-
-    expect(inputBox.length).toBe(2)
+        render(<Contact />)
+        const heading = screen.getByRole("heading")
+    
+        expect(heading).toBeInTheDocument()
+    })
+    
+    test("Should load button in contact us component",()=>{
+        render(<Contact/>)
+    
+        const button = screen.getByText("Submit")
+        expect(button).toBeInTheDocument()
+    })
+    
+    
+    it("Should load placeholder for input in contact us component",()=>{
+        render(<Contact/>)
+    
+        const inputName = screen.getByPlaceholderText("name")
+        expect(inputName).toBeInTheDocument()
+    })
+    
+    it("Should load 2 textbox  in contact us component",()=>{
+        render(<Contact/>)
+    
+        const inputBox = screen.getAllByRole("textbox")
+    
+        expect(inputBox.length).toBe(2)
+    })
 })
